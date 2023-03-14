e7490d78f192b0036fa2f1f9e09d02126bd2708b
tabra26 <tabra26@up.edu>
2023-03-14 12:10:27 -0700

    nickcommit

--- app/src/main/java/com/example/scrabble2_gamestate/GameState.java
+++ app/src/main/java/com/example/scrabble2_gamestate/GameState.java
@@ -11,9 +11,42 @@ import androidx.annotation.NonNull;
  */
 public class GameState {
     private String test;
+    private boolean isSwap;
+    private int playerTurn;
+    private String[] player1Tiles;
+    private String[] player2Tiles;
+    private boolean isHint;
+    private boolean isSkip;
+    private boolean isDoubleLetter;
+    private boolean isDoubleWord;
+    private char[][] board;
+    private int p1Score;
+    private int p2Score;
+    private boolean isTitleScreen;
+    private int iqLevel;
+    private String[] bag;
+    private char letterInPlay;
+    private String hintWord;
+    private boolean isPlayed
 
-    public GameState(String test) {
-        this.test = test;
+    public GameState(String[] p1TilesInit, String[] p2TilesInit, char[][] b, String[] b) {
+        isSwap = false;
+        playerTurn = 1;
+        player1Tiles = p1TilesInit;
+        player2Tiles = p2TilesInit;
+        isHint = false;
+        isSkip = false;
+        isDoubleLetter = false;
+        isDoubleWord = false;
+        board = b;
+        p1Score = 0;
+        p2Score = 0;
+        isTitleScreen = false;
+        iqLevel = 0;
+        bag = b;
+        letterInPlay = ' ';
+        hintWord = " ";
+        isPlayed = false;
     }
 
     @NonNull
