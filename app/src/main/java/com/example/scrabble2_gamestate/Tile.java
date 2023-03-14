df974e4945f0b7ed0195bd40748af0c6ff7f6a62
Jacob <arnez25@up.edu>
2023-03-14 13:15:03 -0700

    Added Tile class

---
+++ app/src/main/java/com/example/scrabble2_gamestate/Tile.java
@@ -0,0 +1,13 @@
+package com.example.scrabble2_gamestate;
+
+public class Tile {
+
+    private char letter;
+    private boolean onBoard;
+    private int score;
+
+    public Tile(char l){
+
+        letter = l;
+    }
+}
