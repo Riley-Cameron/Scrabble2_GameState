83bd2f9382bfd63ae779c006f637e13148f56a9b
Riley-Cameron <astoriacamfam@gmail.com>
2023-03-14 14:03:38 -0700

    makeBag method

--- app/src/main/java/com/example/scrabble2_gamestate/GameState.java
+++ app/src/main/java/com/example/scrabble2_gamestate/GameState.java
@@ -58,7 +58,16 @@ public class GameState {
 
     //A-9, B-2, C-2, D-4, E-12, F-2, G-3, H-2, I-9, J-1, K-1, L-4, M-2, N-6, O-8, P-2, Q-1, R-6, S-4, T-6, U-4, V-2, W-2, X-1, Y-2, Z-1
     public void makeBag(ArrayList<Tile> bag) {
+        char[] letters = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        int[] numLetters = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1};
 
+        for (int i = 0;i<26;i++) {
+            for (int j = 0;j<numLetters[i];j++) {
+                Tile t = new Tile(letters[i]);
+                t.setScore(letterScore.get(letters[i]));
+                bag.add(t);
+            }
+        }
     }
 
     @NonNull
